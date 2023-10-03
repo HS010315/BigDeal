2a47ea0ee31a5e1f8f9622c9c518c5b8db095ee0
pswxxk <sangwook2001@gmail.com>
2023-10-03 21:29:27 +0900

    Feat: "플레이어가 보스 오브젝트 근처로 왔을 때 Shot 오브젝트 활성화"
    Notion-작업-플밍

--- Assets/Scripts/Camera/BossController.cs
+++ Assets/Scripts/Camera/BossController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class BossController : MonoBehaviour
 {
     public AutoCameraMovement cameraMovementScript;
+    public GameObject shotObject; // Shot 오브젝트 프리팹 설정
     public float detectionDistance = 10f; // 카메라 멈춤 거리 설정
 
     void Start()
@@ -18,12 +19,24 @@ public class BossController : MonoBehaviour
             float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
             if (distanceToPlayer < detectionDistance)
             {
-                // 플레이어가 보스 근처로 왔을 때 카메라 이동을 멈춤
+                // 플레이어가 보스 근처로 왔을 때 Shot 오브젝트 활성화
+                if (shotObject != null && !shotObject.activeSelf)
+                {
+                    shotObject.SetActive(true);
+                }
+
+                // 카메라 이동을 멈춤
                 cameraMovementScript.StopCameraMovement();
             }
             else
             {
-                // 플레이어가 멀어졌을 때 카메라 이동을 재개
+                // 플레이어가 멀어졌을 때 Shot 오브젝트 비활성화
+                if (shotObject != null && shotObject.activeSelf)
+                {
+                    shotObject.SetActive(false);
+                }
+
+                // 카메라 이동을 재개
                 cameraMovementScript.ResumeCameraMovement();
             }
         }
