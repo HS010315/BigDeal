20912b32c138b9098a7ab4b62c6c5879765e4095
JongMin0415 <leejm0415@naver.com>
2023-11-21 16:08:05 +0900

    feat"경고"

--- Assets/02.Scripts/Boss/WarningLine.cs
+++ Assets/02.Scripts/Boss/WarningLine.cs
@@ -20,9 +20,15 @@ public class WarningLine : MonoBehaviour
     IEnumerator ShowWarningLine(Vector3 dropPosition)
     {
         lineRenderer.enabled = true;
-        lineRenderer.positionCount = 2;
-        lineRenderer.SetPosition(0, dropPosition);
-        lineRenderer.SetPosition(1, new Vector3(dropPosition.x, 0, dropPosition.z));
+        int numberOfPoints = 100;
+        lineRenderer.positionCount = numberOfPoints;
+
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            float t = i / (float)(numberOfPoints - 1);
+            Vector3 pointPosition = Vector3.Lerp(dropPosition, new Vector3(dropPosition.x, 0, dropPosition.z), t) - Vector3.up * Mathf.Lerp(0, 10, t);
+            lineRenderer.SetPosition(i, pointPosition);
+        }
 
         yield return new WaitForSeconds(warningDuration);
 
