23d649325e85a93ebf1f912988249916d0df2492
JongMin0415 <leejm0415@naver.com>
2023-12-05 18:24:33 +0900

    Fix : "bullet 수정 "

--- Assets/02.Scripts/Enemy/Bullet.cs
+++ Assets/02.Scripts/Enemy/Bullet.cs
@@ -11,13 +11,22 @@ public class Bullet : MonoBehaviour
 
     void Update()
     {
-        // 총알의 현재 위치를 화면 좌표로 변환
-        Vector3 screenPoint = mainCamera.WorldToViewportPoint(transform.position);
-
-        // 만약 총알이 카메라 시야 밖으로 나가면 삭제
-        if (screenPoint.x < 0 || screenPoint.x > 1 || screenPoint.y < 0 || screenPoint.y > 1)
+        // 메인 카메라가 존재하는지 확인
+        if (mainCamera != null)
         {
-            Destroy(gameObject);
+            // 총알의 현재 위치를 화면 좌표로 변환
+            Vector3 screenPoint = mainCamera.WorldToViewportPoint(transform.position);
+
+            // 만약 총알이 카메라 시야 밖으로 나가면 삭제
+            if (screenPoint.x < 0 || screenPoint.x > 1 || screenPoint.y < 0 || screenPoint.y > 1)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            // 메인 카메라가 없는 경우 예외 처리 또는 적절한 대책을 수행
+            Debug.LogWarning("Main Camera is missing!");
         }
     }
 }
