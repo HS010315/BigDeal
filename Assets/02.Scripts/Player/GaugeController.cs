8d41bf5894a56803f6fd6616462458d2839f46ea
JongMin0415 <leejm0415@naver.com>
2023-12-07 13:13:19 +0900

    fix : " 게이지 사용량 인스펙터창에서 조절"

--- Assets/02.Scripts/Player/GaugeController.cs
+++ Assets/02.Scripts/Player/GaugeController.cs
@@ -3,14 +3,17 @@ using UnityEngine.UI;
 
 public class GaugeController : MonoBehaviour
 {
-    public Slider gaugeSlider; // Unity 에디터에서 연결할 Slider UI 요소
+    public Slider gaugeSlider; // Connect Slider UI element in the Unity Editor
     private bool isShiftKeyPressed = false;
+
+    // 각 스테이지에서 사용할 게이지 사용량을 인스펙터 창에서 설정
+    public float usageRate = 20.0f;
 
     private void Start()
     {
         gaugeSlider.minValue = 0;
         gaugeSlider.maxValue = 100;
-        gaugeSlider.value = 100; // 초기 게이지 값 설정
+        gaugeSlider.value = 100; // Set the initial gauge value
     }
 
     void Update()
@@ -18,7 +21,7 @@ public class GaugeController : MonoBehaviour
         // 왼쪽 쉬프트 키가 눌렸는지 확인
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            // 왼쪽 쉬프트 키가 눌려 있으면 게이지를 초당 10씩 감소시킴
+            // 왼쪽 쉬프트 키가 눌려 있으면 게이지를 감소시킴
             isShiftKeyPressed = true;
         }
         else
@@ -26,20 +29,17 @@ public class GaugeController : MonoBehaviour
             isShiftKeyPressed = false;
         }
 
-        // 왼쪽 쉬프트 키가 눌렸을 때 게이지를 감소시키고, 떼면 게이지를 증가시킴
+        // 설정된 usageRate에 따라 게이지 사용량을 적용
         if (isShiftKeyPressed)
         {
-            gaugeSlider.value -= 20 * Time.deltaTime; // 초당 10씩 감소시킴 (Time.deltaTime을 곱해 초당 감소량으로 변환)
+            gaugeSlider.value -= usageRate * Time.deltaTime;
         }
         else
         {
-            gaugeSlider.value += 3 * Time.deltaTime; // 왼쪽 쉬프트 키가 떼어져 있을 때 초당 증가시킴
+            gaugeSlider.value += 3 * Time.deltaTime;
         }
 
         // 게이지가 범위를 벗어나지 않도록 보정
         gaugeSlider.value = Mathf.Clamp(gaugeSlider.value, gaugeSlider.minValue, gaugeSlider.maxValue);
-
-        
-       
     }
 }
